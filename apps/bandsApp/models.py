4dfef2efaacb38e50c9c4c652b3fa8c3bbbdea9a
sheetaldesai <sheetaldesai1@gmail.com>
2017-10-21 15:41:03 -0700

    added new datafields and functionality

--- apps/bandsApp/models.py
+++ apps/bandsApp/models.py
@@ -9,15 +9,15 @@ class User(models.Model):
     username = models.CharField(max_length=255)
     password = models.CharField(max_length=8)
     email = models.EmailField()
-    creditCard = models.CharField(max_length=16, null= True)
-    shipping_street_address = models.CharField(max_length=255, null=True)
-    shipping_city = models.CharField(max_length=255, null=True)
-    shipping_state = models.CharField(max_length=2, null=True)
-    shipping_country = models.CharField(max_length=50, null=True)
-    billing_street_address = models.CharField(max_length=255, null=True)
-    billing_city = models.CharField(max_length=255, null=True)
-    billing_state = models.CharField(max_length=2, null=True)
-    billing_country = models.CharField(max_length=50, null=True)
+    creditCard = models.CharField(max_length=16, null= True, blank=True)
+    shipping_street_address = models.CharField(max_length=255, null=True, blank=True)
+    shipping_city = models.CharField(max_length=255, null=True, blank=True)
+    shipping_state = models.CharField(max_length=2, null=True, blank=True)
+    shipping_country = models.CharField(max_length=50, null=True, blank=True)
+    billing_street_address = models.CharField(max_length=255, null=True, blank=True)
+    billing_city = models.CharField(max_length=255, null=True, blank=True)
+    billing_state = models.CharField(max_length=2, null=True, blank=True)
+    billing_country = models.CharField(max_length=50, null=True, blank=True)
   
     # objects = UserManager()
     def __str__(self):
@@ -28,6 +28,7 @@ class Vendor(models.Model):
     email = models.EmailField() # for contacting the vendor, about specific product questions
     vendorType = models.CharField(max_length=20) # localArtist, brand, charity
     bio = models.CharField(max_length=255)  # vendor short bio.
+    imageUrl = models.CharField(max_length=255)
 
 
 class Band(models.Model):
@@ -36,13 +37,18 @@ class Band(models.Model):
     size = models.CharField(max_length=10)
     imageUrl = models.CharField(max_length=255)
     vendor = models.ForeignKey('Vendor', related_name = "bands")
-    
+    isFeatured = models.BooleanField(default=False)
+    price = models.FloatField(default=0)
     def __str__(self):
-        return "{} {}".format(self.name, self.creator.name) 
+        return "{} {}".format(self.name, self.vendor.name) 
 
 class Order(models.Model):
     bands = models.ManyToManyField(Band)
-    user = models.ForeignKey('user', related_name = "orders")
+    user = models.ForeignKey('user', related_name = "orders", null=True, blank=True)
+    session_id = models.CharField(max_length=50,null=True, blank=True)
     status = models.CharField(max_length=10)
     created_at = models.DateTimeField('date created')
-    updated_at = models.DateTimeField('date created')
+    updated_at = models.DateTimeField('date updated')
+    
+    def __str__(self):
+        return "{} {}".format(self.id, self.status) 
--- apps/bandsApp/views.py
+++ apps/bandsApp/views.py
@@ -13,23 +13,59 @@ from django.contrib import messages
 
 # Display home page
 def index (request) :  
-    context = {}
+    
+    numCartItems = 0
+
+    if not request.session.session_key:
+        request.session.save()
+    session_key = request.session.session_key   
+    print session_key
+
+    # Find if the session key exists in orders db 
+    order = Order.objects.get(session_id=session_key)
+    print order
+
+    if (order) :
+        request.session['order_id'] = order.id
+        bands = order.bands.all()
+        numCartItems = len(bands)
+        print numCartItems
+    
+    # Get artists featured items
+    artistFeaturedBands = Band.objects.filter(vendor__vendorType='artist', isFeatured='True')
+    print artistFeaturedBands
+
+    # Get exclusive featured items
+    exclusiveFeaturedBands = Band.objects.filter(vendor__vendorType='exclusive', isFeatured='True')
+    print exclusiveFeaturedBands
+
+    # Get exclusive featured items    
+    charityFeaturedBands = Band.objects.filter(vendor__vendorType='charity', isFeatured='True')
+    print charityFeaturedBands
+
+    context = {'numCartItems':numCartItems, 'featuredArtist':artistFeaturedBands, 'featuredExclusive':exclusiveFeaturedBands, 'featuredCharity':charityFeaturedBands}
     return render(request, 'bandsApp/index.html', context)
 
 # Display all the bands by local artists. 
 def artists (request) :  
-    context = {}
-    return render(request, 'bandsApp/artistsBands.html', context)
+    bands = Band.objects.filter(vendor__vendorType='artist')
+    print bands
+    context = {'bands':bands, 'type':'artist'}
+    return render(request, 'bandsApp/vendorBands.html', context)
 
 # Display all the bands by exclusive brands. 
 def exclusives (request) :  
-    context = {}
-    return render(request, 'bandsApp/exclusiveBands.html', context)
+    bands = Band.objects.filter(vendor__vendorType='exclusive')
+    print bands
+    context = {'bands':bands, 'type':'exclusive'}
+    return render(request, 'bandsApp/vendorBands.html', context)
 
 # Display all the bands by charities. 
 def cause (request) :  
-    context = {}
-    return render(request, 'bandsApp/charityBands.html', context)
+    bands = Band.objects.filter(vendor__vendorType='charity')
+    print bands
+    context = {'bands':bands, 'type':'charity'}
+    return render(request, 'bandsApp/vendorBands.html', context)
 
 # Display the search results 
 def search (request) :  
@@ -39,11 +75,17 @@ def search (request) :
 # Display shopping cart 
 def showCart (request, orderId) :  
     context = {'orderId':orderId}
+    order = Order.objects.get(pk=orderId)
+    bands = order.bands.all()
+    totalPrice = 0
+    for band in bands :
+        totalPrice += band.price
     return render(request, 'bandsApp/shoppingCart.html', context)
 
-# Edit shopping cart 
+# Edit shopping cart. some problem here. i need to fix it 
 def editCart (request, orderId) :  
     context = {'orderId':orderId}
+    # Edit the order table entry and redirect to showCart 
     return redirect(reverse('bands:cart'))
 
 
